c8f9416d7459fb551dbb1738fe0bbb314d7184ec
tang <jackeys@163.com>
2020-07-10 09:50:10 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,8 +3,8 @@ const PRECACHE = 'precache-v1';
 const RUNTIME = 'runtime';
 const HOSTNAME_WHITELIST = [
   self.location.hostname,
-  "huangxuan.me",
-  "yanshuo.io",
+  "tangs.space",
+ 
   "cdnjs.cloudflare.com"
 ]
 
